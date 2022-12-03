33860e16bfd14ebf1f6dd0f947c95aceb572fe98
Shalia Wynn <swynn@accuradio.com>
2022-12-03 03:25:12 -0600

    Completed part 2.

--- day02/day02.py
+++ day02/day02.py
@@ -1,5 +1,6 @@
 # Part 1
 # Answer: 13675
+MAX_SHAPE_NUMS = 3
 shape_values = {"rock": 1, "paper": 2, "scissors": 3}
 opponent_mappings = {"A": "rock", "B": "paper", "C": "scissors"}
 player_mappings = {"X": "rock", "Y": "paper", "Z": "scissors"}
@@ -7,14 +8,12 @@ player_mappings = {"X": "rock", "Y": "paper", "Z": "scissors"}
 with open("input.txt") as f_handle:
 	f_content = f_handle.readlines()
 
-# Test data
-# f_content = ["A Y", "B X", "C Z"]
-
 total_player_match_score = 0
 for line in f_content:
-	player_round_score = 0
 	line = line.strip()
+
 	round_responses = line.split(" ")
+
 	opponent_round_response = opponent_mappings[round_responses[0]]
 	player_round_response = player_mappings[round_responses[1]]
 
@@ -23,32 +22,78 @@ for line in f_content:
 
 	player_round_score = player_round_value
 
-	if player_round_value == opponent_round_value:
+	match_score = (player_round_value - opponent_round_value) % MAX_SHAPE_NUMS
+	if match_score == 0:
 		# It was a draw.
 		player_round_score += 3
 
-	elif (player_round_value > opponent_round_value) and (abs(player_round_value - opponent_round_value) == 1):
-		# Player won (scissors beats paper beats rock).
+	elif match_score <= 1:
+		# Player won.
 		player_round_score += 6
 
-	elif (player_round_value < opponent_round_value) and (abs(player_round_value - opponent_round_value) == 2):
-		# Player won (rock beats scissors).
-		player_round_score += 6
 	else:
 		# Player lost.
 		pass
-
-	# Test
-	# print(round_responses)
-	# print(opponent_round_response)
-	# print(player_round_response)
-	# print(opponent_round_value)
-	# print(player_round_value)
-	# print(player_round_score)
 
 	total_player_match_score += player_round_score
 
 print(total_player_match_score)
 
 # Part 2
-# Answer: 14539 13525
+# Answer: 14184
+from enum import Enum
+player_mappings = {"X": "lose", "Y": "draw", "Z": "win"}
+
+class PlayerDirectives(Enum):
+	DRAW = "draw"
+	WIN = "win"
+	LOSE = "lose"
+
+total_player_match_score = 0
+for line in f_content:
+	line = line.strip()
+
+	round_responses = line.split(" ")
+
+	opponent_round_response = opponent_mappings[round_responses[0]]
+	player_round_state = player_mappings[round_responses[1]]
+	opponent_round_value = shape_values[opponent_round_response]
+
+	player_round_value = 0
+	player_round_score = 0
+
+	if PlayerDirectives.DRAW.value == player_round_state:
+		# It was a draw.
+		for shape, val in shape_values.items():
+			if (val - opponent_round_value) % MAX_SHAPE_NUMS == 0:
+				player_round_response = shape
+				player_round_value = val
+				break
+
+		player_round_score += player_round_value
+		player_round_score += 3
+
+	elif PlayerDirectives.WIN.value == player_round_state:
+		# Player won.
+		for shape, val in shape_values.items():
+			if (val - opponent_round_value) % MAX_SHAPE_NUMS != 0 and (val - opponent_round_value) % MAX_SHAPE_NUMS <= 1:
+				player_round_response = shape
+				player_round_value = val
+				break
+
+		player_round_score += player_round_value
+		player_round_score += 6
+
+	else:
+		# Player lost.
+		for shape, val in shape_values.items():
+			if (val - opponent_round_value) % MAX_SHAPE_NUMS > 1:
+				player_round_response = shape
+				player_round_value = val
+				break
+
+		player_round_score += player_round_value
+
+	total_player_match_score += player_round_score
+
+print(total_player_match_score)
